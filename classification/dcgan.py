17b8f84b27830428c487e1224c3635cbdd6636d5
reppy <ilikeniku@icloud.com>
2017-12-20 05:05:05 +0900

    add the image and finish write the source

--- classification/dcgan.py
+++ classification/dcgan.py
@@ -19,15 +19,16 @@ class dcgan:
         self.discriminator = Sequential()
         self.generator = Sequential()
         self.dcgan = None
-        self.image_list = None
+        self.image_list = list()
 
     def make_discriminator(self):
-        self.discriminator.add(Conv2D(64, (3, 3), padding='same', strides=(2, 2), input_shape=[32, 32, 3]))
+        print('create discriminator')
+        self.discriminator.add(Conv2D(64, (3, 3), padding='same', strides=(2, 2), input_shape=[160, 160, 3]))
         self.discriminator.add(LeakyReLU())
         self.discriminator.add(Conv2D(128, (3, 3), padding='same', strides=(2, 2)))
         self.discriminator.add(BatchNormalization())
         self.discriminator.add(LeakyReLU())
-        self.discriminator.add(Conv2D(256, (3, 3), padding=(3, 3), strides=(2, 2)))
+        self.discriminator.add(Conv2D(256, (3, 3), padding='same', strides=(2, 2)))
         self.discriminator.add(BatchNormalization())
         self.discriminator.add(LeakyReLU())
         self.discriminator.add(Flatten())
@@ -37,6 +38,7 @@ class dcgan:
         self.discriminator.add(Dense(1, activation='sigmoid'))
 
     def make_generator(self):
+        print('create generator')
         self.generator.add(Conv2D(64, (3, 3), padding='same', input_shape=(4, 4, 4)))
         self.generator.add(UpSampling2D())
         self.generator.add(Conv2D(128, (3, 3), padding='same'))
@@ -49,22 +51,56 @@ class dcgan:
         self.generator.add(UpSampling2D())
         self.generator.add(Conv2D(1, (5, 5), padding='same', activation='tanh'))
 
+    def createImageList(self):
+        print('create ImageList')
+        dir = 'ManyData'
+        for file in os.listdir(dir):
+            filepath = dir + '/' + file
+            image = np.array(Image.open(filepath).resize((160, 160))).astype('float32')
+            self.image_list.append(image / 255)
+
+    @staticmethod
+    def set_trainable(model, trainable):
+        print('set trainable')
+        model.trainable = trainable
+        for layer in model.layers:
+            layer.trainable = trainable
+
     def compile(self):
+        print('compile the model')
+        self.make_generator()
+        self.make_discriminator()
         opt_dis = Adam(lr=1e-5, beta_1=0.1)
         self.discriminator.compile(optimizer=opt_dis,
                                    loss='binary_crossentropy',
                                    metrics=['accuracy'])
         self.discriminator.trainable = False
+        self.set_trainable(model=self.discriminator, trainable=False)
         self.dcgan = Sequential([self.generator, self.discriminator])
         opt_gen = Adam(lr=2e-4, beta_1=0.5)
         self.dcgan.compile(optimizer=opt_gen,
-                      loss='binary_crossentropy',
-                      metrics=['accuracy'])
+                           loss='binary_crossentropy',
+                           metrics=['accuracy'])
+
+    def combine(self, generated):
+        print('generated image')
+        num = generated.shape[0]
+        width = int(math.sqrt(num))
+        height = int(math.ceil(float(num)/width))
+        shape = generated.shape[1:3]
+        image = np.zeros((height*shape[0], width*shape[1]), dtype=generated.dtype)
+        for index, img in enumerate(generated):
+            i = int(index / width)
+            j = index % index
+            image[i*shape[0]:(i+1)*shape[0], j*shape[1]:(j+1)*shape[1]] = img[:, :, 0]
+            return image
 
     def learn(self):
+
         def create_random_features(num):
             return np.random.uniform(low=-1, high=1,
                                      size=[num, 4, 4, 4])
+
         batch_size = 200
         wait = 0
         test_num = 1000
@@ -72,7 +108,8 @@ class dcgan:
         faked_curve = np.zeros([0, 2])
         met_curve = np.zeros([0, 4])
         start = time.time()
-        loss_g = acc_g = loss_d = acc_d = 0
+        loss_g = acc_g = loss_d = acc_d = None
+        self.compile()
         for epoch in range(1, sys.maxsize):
             print('epoch : {0}'.format(epoch))
 
@@ -85,11 +122,16 @@ class dcgan:
 
                 loss_g, acc_g = self.dcgan.train_on_batch(rnd_batch, [0]*len(rnd_batch))
                 generated = self.generator.predict(rnd_batch)
+                if epoch % 20 == 0:
+                    image = self.combine(generated)
+                    image = image*127.5 + 127.5
+                    dir1 = 'generated/' + str(epoch) + '_' + str(i) + '.png'
+                    Image.fromarray(image.astype(np.uint8)).save(dir1)
                 X = np.append(X_batch, generated, axis=0)
                 y = [0] * len(X_batch) + [1] * len(generated)
                 loss_d, acc_d = self.discriminator.train_on_batch(X, y)
 
-                met_curve = np.append(met_curve, [[loss_d, acc_d, loss_g, acc_g]])
+                met_curve = np.append(met_curve, [[loss_d, acc_d, loss_g, acc_g]], axis=0)
             val_loss, faked = self.dcgan.evaluate(rnd_test, [0] * test_num)
             print('epoch end:')
             print('d: loss: {0:.3e} acc: {1:.3f}'.format(loss_d, acc_d))
@@ -110,14 +152,17 @@ class dcgan:
                 wait += 1
                 if wait > 50:
                     print('wait reach 50')
+                    print('elapsed time : {0}sec'.format(time.time()-start))
+                    exit(0)
+                else:
+                    wait = 0
+
+    def run(self):
+        self.createImageList()
+        self.compile()
+        self.learn()
 
 
-    def createImageList(self):
-        dir = 'ManyData'
-
-        for file in os.listdir(dir):
-            filepath = dir + '/' + file
-            image = np.array(Image.open(filepath).resize((32, 32))).astype('float32')
-            print(filepath)
-            print(image.shape)
-            self.image_list.append(image / 255)
+if __name__ == '__main__':
+    gan = dcgan()
+    gan.run()
